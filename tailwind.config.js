f59a2e87e71e81a010f93176a6c5e2a02ea79d9b
Matias <matiasc.02.2003@gmail.com>
2023-03-29 16:32:04 -0300

    feat(src/view/ LandingPage) Landing Page and add colors tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "base" : "#010922",
+        "light" : "#A9CAEF",
+        "primary" : "#8181D7",
+        "secondary" : "#75D4DB"
+
+      }
+    },
   },
   plugins: [],
 };

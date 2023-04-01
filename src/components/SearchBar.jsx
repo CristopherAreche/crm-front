35360165daaffc2cd8087edfce198e33a410f5e2
Gxspxr <sernagas1234@gmail.com>
2023-04-01 19:31:20 -0300

    SearchBar function in progress

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,41 +1,47 @@
 import { RiSearchLine } from "react-icons/ri";
 import { useState } from "react";
-import { getClients, getClientName } from "../services/clientsServices";
+import { getClientName, getClients } from "../services/clientsServices";
 import { useDispatch } from "react-redux";
+
 const SearchBar = () => {
   const [search, setSearch] = useState("");
-
   const dispatch = useDispatch();
 
-  const handleChange = (event) => {
-    if (event.target.value === "") {
-      dispatch(getClients());
+  const handleSearch = async () => {
+    try {
+      if (search === "") {
+        dispatch(getClients());
+        return;
+      }
+      const clients = await dispatch(getClientName(search)).unwrap();
+      if (clients.length === 0) {
+        console.log(`No se encontraron clientes con el nombre "${search}".`);
+      }
+    } catch (error) {
+      console.error(error);
     }
-    setSearch(event.target.value);
-  };
-
-  const handleSearch = () => {
-    dispatch(getClientName(search));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    dispatch(getClientName(search));
+    handleSearch();
   };
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
-      if (search.length !== 0) {
-        handleSearch();
-      } else dispatch(getClients());
+      handleSearch();
     }
+  };
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
   };
 
   return (
     <div className="relative">
       <input
         placeholder="Buscar..."
-        className="py-2 pl-10 pr-6 rounded-full w-80 bg-base-light/40 outline-none "
+        className="py-2 pl-10 pr-6 rounded-full w-80 bg-base-light/40 outline-none"
         type="text"
         value={search}
         onChange={handleChange}
@@ -48,3 +54,4 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
+

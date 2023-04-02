548a8e3132184e84a1844ca459d5f37dc90f29b2
Gxspxr <sernagas1234@gmail.com>
2023-04-01 21:50:39 -0300

    SearchBar completed :)

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,40 +1,35 @@
-import { RiSearchLine } from "react-icons/ri";
 import { useState } from "react";
-import { getClientName, getClients } from "../services/clientsServices";
-import { useDispatch } from "react-redux";
+import { getClients} from "../services/clientsServices";
+import { searchClients } from "../app/store/clientSlice";
+import { useDispatch, useSelector} from "react-redux";
 
 const SearchBar = () => {
-  const [search, setSearch] = useState("");
-  const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSearch = async () => {
-    try {
-      if (search === "") {
-        dispatch(getClients());
-        return;
-      }
-      const clients = await dispatch(getClientName(search)).unwrap();
-      if (clients.length === 0) {
-        console.log(`No se encontraron clientes con el nombre "${search}".`);
-      }
-    } catch (error) {
-      console.error(error);
+  const dispatch = useDispatch();
+  
+  const clients = useSelector((state) => state.clients.clients);
+
+  const handleChange = (event) => {
+    if(event.target.value===""){
+      dispatch(getClients())
     }
+    setSearchTerm(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    handleSearch();
+  const handleSearch = () => {
+    dispatch(searchClients(clients.filter((client) =>
+      client.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )));
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      handleSearch();
+    if (event.key === 'Enter') {
+      if(searchTerm.length!==0){
+        handleSearch();
+      }
+      else dispatch((getClients()));
     }
-  };
-
-  const handleChange = (event) => {
-    setSearch(event.target.value);
   };
 
   return (
@@ -43,12 +38,10 @@ const SearchBar = () => {
         placeholder="Buscar..."
         className="py-2 pl-10 pr-6 rounded-full w-80 bg-base-light/40 outline-none"
         type="text"
-        value={search}
+        value={searchTerm}
         onChange={handleChange}
-        onSubmit={handleSubmit}
         onKeyPress={handleKeyPress}
       />
-      <RiSearchLine className="absolute top-1/2 -translate-y-1/2 left-4 text-xl text-light/70" />
     </div>
   );
 };

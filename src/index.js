ebea98381518e4068f213e8508fedd3753758559
Gxspxr <sernagas1234@gmail.com>
2023-04-05 14:16:08 -0300

    Login y register semi-configurado

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Auth0Provider domain="dev-tthzw3uiioypptkj.us.auth0.com" clientId="flqFB9wWez8Ld0z3i40ZYJhmO9LRvEHU"  redirectUri={`${window.location.origin}/vendedor`} >
   <Provider store={store}>
-
     <BrowserRouter>
       <App />
     </BrowserRouter>

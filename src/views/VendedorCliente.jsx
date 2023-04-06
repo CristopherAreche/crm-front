9bdd67c4253e5be3096516b93c95ae70847a09d4
Matias <matiasc.02.2003@gmail.com>
2023-04-06 14:26:57 -0300

    refactor(features/clientSlice) add logic of reducers in handlers foulder

--- src/views/VendedorCliente.jsx
+++ src/views/VendedorCliente.jsx
@@ -5,12 +5,12 @@ import FilterTop from "../components/FilterTop";
 
 const VendedorCliente = () => {
   return (
-    <>
+    <section className="py-6 px-12 z-[2] ">
       <Header />
-       <FilterTop />
+      <FilterTop />
       <ClientList />
       <FilterBottom />
-    </>
+    </section>
   );
 };
 
--- src/views/VendedorDetallesCliente.jsx
+++ src/views/VendedorDetallesCliente.jsx
@@ -1,33 +1,24 @@
 import React from "react";
-import RegisterActivityTable from "../components/RegisterActivityTable";
 import TaskList from "../components/TaskList";
-import DateFilter from "../components/DateFilter";
-import ActivitiesFilter from "../components/ActivitiesFilter";
+
 import SideBar from "../components/SideBar";
 import spotlight1 from "../assets/svg/Spotlight1.svg";
 import spotlight2 from "../assets/svg/Spotlight2.svg";
-import { RiBarChart2Line } from "react-icons/ri";
+import ActivitiesList from "../components/ActivitiesList";
 
 const VendedorDetallesCliente = () => {
   return (
-    <main className="bg-base h-screen grid grid-cols-8 ">
+    <section className="bg-base h-screen ">
       <SideBar typeSidebar="client-detail" />
       {/* View Client Detail */}
-      <section className="col-span-6 w-[100vw] lg:w-auto  z-40 h-full overflow-y-auto px-8 py-6">
-        <TaskList />
-        <section className="flex flex-col gap-y-2">
-          <h4 className="text-2xl text-light font-medium flex gap-x-4 items-center mb-2">
-            <RiBarChart2Line className="text-4xl" />
-            Actividades
-          </h4>
-          <div className="flex gap-x-8">
-            <DateFilter />
-            <ActivitiesFilter />
-          </div>
-
-          <RegisterActivityTable />
+      <main className="lg:pl-72 w-[100vw] lg:w-auto  h-full overflow-y-auto flex flex-col">
+        <section className="py-6 px-12  z-[2]">
+          <TaskList />
+          <ActivitiesList />
         </section>
-      </section>
+      </main>
+
+      {/* Luces */}
       <img
         src={spotlight1}
         className="absolute top-0 left-0 opacity-80 z-[1]"
@@ -38,7 +29,7 @@ const VendedorDetallesCliente = () => {
         className="absolute bottom-0 right-0 opacity-80 z-[1]"
         alt="light glow"
       />
-    </main>
+    </section>
   );
 };
 

a7c70f02446080f7ac6d8508beb48b0e1a7316db
CristopherAreche <cristopherareche764@gmail.com>
2023-04-06 18:56:07 -0400

    ClientDetails stilo arreglado y boton volver agregado

--- src/components/sidebars/ClientDetailSideBar.jsx
+++ src/components/sidebars/ClientDetailSideBar.jsx
@@ -6,11 +6,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getClient } from "../../services/clientsServices";
 import { cleanDetail } from "../../app/features/clientSlice";
+import { Link } from "react-router-dom";
+import { RiLogoutCircleRLine } from "react-icons/ri";
 
 const ClientDetailSideBar = () => {
   const dispatch = useDispatch();
   const { clientDetail } = useSelector((state) => state.clients);
   const { id } = useParams();
+  const role = useSelector((state) => state.clients.clientRole);
 
   useEffect(() => {
     dispatch(getClient(id));
@@ -19,21 +22,28 @@ const ClientDetailSideBar = () => {
   }, [dispatch, id]);
 
   return (
-    <section className="flex items-center flex-col gap-y-3 px-8 w-full">
-      <div className="relative">
+    <section className="bg-base-light/80 lg:bg-base-light/40 col-start-1 col-end-3  flex flex-col gap-y-8 justify-center items-center  fixed lg:static  h-screen z-50 transition-all duration-200 lg:left-0">
+      <div className=" flex flex-col justify-center items-center w-full">
         <img
           src="https://via.placeholder.com/150"
           alt="placeholder"
-          className="w-28 h-28 rounded-full "
+          className="w-28 h-28 rounded-full mb-8"
         />
-        <div
-          className={`${
-            clientDetail.enable
-              ? "bg-green-400 shadow-green-400/10"
-              : "bg-gray-400 shadow-gray-400/10"
-          } text-xs text-center rounded-full py-1 px-2 font-bold text-white shadow-md absolute bottom-0 right-0`}
-        >
-          {clientDetail.enable ? "Habilitado" : "Deshabilitado"}
+        <div className="flex gap-x-2 w-[100%] justify-center">
+          <div
+            className={`${
+              clientDetail.enable
+                ? "bg-green-400 shadow-green-400/10"
+                : "bg-gray-400 shadow-gray-400/10"
+            } text-xs flex justify-center items-center rounded-full py-1 px-2 font-bold text-black shadow-md bottom-0 right-0`}
+          >
+            {clientDetail.enable ? "Habilitado" : "Deshabilitado"}
+          </div>
+          {clientDetail.vip === true ? (
+            <div className=" text-center bg-yellow-400 py-2 px-4  rounded-full font-medium text-base-light shadow-md shadow-yellow-400/10">
+              VIP
+            </div>
+          ) : null}
         </div>
       </div>
       <h4 className="text-light text-2xl  text-center font-medium">
@@ -56,12 +66,23 @@ const ClientDetailSideBar = () => {
         </p>
         <p className="text-light/80 font-medium ">Total pagado</p>
       </div>
-
-      {clientDetail.vip === true ? (
-        <div className="bg-yellow-400 py-2 px-4  rounded-lg font-medium text-base-light shadow-md shadow-yellow-400/10">
-          VIP
-        </div>
-      ) : null}
+      {role !== "admin" ? (
+        <Link
+          to="/dashboard/all_clients"
+          className="flex px-12 py-2  active:scale-95 active:bg-light/20 gap-x-6 items-center text-lg text-gray-300 font-medium   cursor-pointer  hover:text-gray-100 transition-all"
+        >
+          <RiLogoutCircleRLine className="text-2xl text-secondary" />
+          Volver
+        </Link>
+      ) : (
+        <Link
+          to="#"
+          className="flex px-12 py-2  active:scale-95 active:bg-light/20 gap-x-6 items-center text-lg text-gray-300 font-medium   cursor-pointer  hover:text-gray-100 transition-all"
+        >
+          <RiLogoutCircleRLine className="text-2xl text-secondary" />
+          Volver
+        </Link>
+      )}
     </section>
   );
 };

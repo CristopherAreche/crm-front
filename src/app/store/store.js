7ff8dda491fc1a62db55c1836493386937859bfb
Gxspxr <sernagas1234@gmail.com>
2023-04-07 11:05:25 -0300

    Auth redux

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import clientReducer from "../features/clientSlice";
 import activityReducer from "../features/clientActivitiesSlice";
 import  sellerSlice  from "../features/sellerSlice";
+import authSlice from "../features/authSlice";
 
 export const store = configureStore({
   reducer: {
     clients: clientReducer,
     activities: activityReducer,
     sellers: sellerSlice,
+    auth: authSlice,
   },
 });

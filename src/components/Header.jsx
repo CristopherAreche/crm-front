a57decc382112b33dce47720ed93d1ddb3f3aa2e
Macarr <rodriguezruccimacarena@gmail.com>
2023-04-06 21:33:57 -0300

    Formularios para crear y editar vendedores, slice y services

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import SearchBar from "./SearchBar";
 
 const Header = ({ imageSrc, mainText, data, onSearch, onReset }) => {
-  //SEARCH BAR ES UN COMPONENTE REUTILIZABLE
+  //SEARCHBAR Y HEADER ---> COMPONENTES REUTILIZABLES
   return (
     <header className="text-white w-full flex flex-col lg:flex-row items-center  lg:justify-between gap-y-2 mb-6">
       <h2 className=" text-3xl  text-light font-bold">{mainText}</h2>
--- src/views/dashboard/Sellers.jsx
+++ src/views/dashboard/Sellers.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 import SideBar from "../../components/sidebars/SideBar";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import BossFilterTop from "../../components/bossComponents/BossFilterTop";
 import SellersList from "../../components/bossComponents/SellersList";
+import BossFilterBottom from "../../components/bossComponents/BossFilterBottom";
+import { resetSellers, searchSellers } from "../../app/features/sellerSlice";
+import Header from "../../components/Header";
 
 const Sellers = () => {
   const role = useSelector((state) => state.clients.clientRole);
+  const dispatch = useDispatch();
+  const sellers = useSelector((state) => state.sellers.sellers);
   return (
-    <main className="bg-base h-screen text-white grid grid-cols-10">
+    <main className="bg-base h-screen text-white grid grid-cols-10 ">
       <SideBar />
       {role !== "admin" ? (
         <div className="text-white">
           Inventory --- aqui va el inventario desde la vista del Vendedor.
         </div>
       ) : (
-        <main className=" px-6 w-full py-[1em] col-start-3 col-end-11 h-[100vh] overflow-y-auto z-40 flex flex-col gap-2 lg:w-auto justify-center items-center">
+        <main className=" px-6 w-full py-[1em] col-start-3 col-end-11 h-[100vh] overflow-y-auto z-40 flex flex-col gap-2 lg:w-auto justify-start items-center">
           <Outlet />
+          <Header
+            mainText={"VENDEDORES"}
+            data={sellers}
+            onSearch={(filteredSellers) =>
+              dispatch(searchSellers(filteredSellers))
+            }
+            onReset={() => dispatch(resetSellers())}
+          />
           <BossFilterTop />
           <SellersList />
-          <div className=" h-[5em] w-full bg-green-400 flex justify-center items-center text-black">
-            Agregar | Eliminar
-          </div>
+          <BossFilterBottom />
         </main>
       )}
     </main>

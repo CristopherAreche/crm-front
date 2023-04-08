f34e4f6d98999a7f4180f260dea56c942a3f29b8
Matias <matiasc.02.2003@gmail.com>
2023-04-08 19:30:57 -0300

    Summary seller view semi-completed

--- src/components/shared/PrincipalInformation.jsx
+++ src/components/shared/PrincipalInformation.jsx
@@ -4,8 +4,10 @@ const PrincipalInformation = ({data}) => {
   return (
     <section className='flex flex-row lg:flex-col gap-x-4 lg:gap-y-2 items-center lg:items-start'>
         <img src='https://cdn-icons-png.flaticon.com/512/219/219983.png' alt='placeholde' className='h-32 w-32'/>
-        <h4 className='text-2xl font-medium text-light'>{data.name}</h4>
-        <p className='text-light/80'>{data.email}</p>
+         <div className='flex flex-col gap-y-2'>
+          <h4 className='text-2xl font-medium text-light'>{data.name}</h4>
+            <p className='text-light/80'>{data.email}</p>
+         </div>
     </section>
   )
 }

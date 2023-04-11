8de2961e3015d1830f761211fb0fd0b4c294f666
CristopherAreche <cristopherareche764@gmail.com>
2023-04-11 18:03:51 -0400

    otros botones

--- src/components/FilterBottom.jsx
+++ src/components/FilterBottom.jsx
@@ -15,8 +15,10 @@ const FilterBottom = () => {
 
   const dispatch = useDispatch();
   const { clientSelected } = useSelector((state) => state.clients);
-  const onDisabled = () => dispatch(toggleStatusClient({clientSelected, enable : false}))
-  const onEnabled = () => dispatch(toggleStatusClient({clientSelected, enable : true}));
+  const onDisabled = () =>
+    dispatch(toggleStatusClient({ clientSelected, enable: false }));
+  const onEnabled = () =>
+    dispatch(toggleStatusClient({ clientSelected, enable: true }));
   const onCreateClient = () => {
     dispatch(cleanClientSelect());
     setShowModal(true);
@@ -25,28 +27,40 @@ const FilterBottom = () => {
   return (
     <section className=" text-white text-bold flex justify-evenly w-full  items-center rounded-md flex-wrap gap-4 pt-8">
       <button
-        className=" rounded-xl py-2 px-3 shadow-emerald-400/20 hover:scale-[1.03] hover:bg-emerald-400/80 transition-all shadow-md bg-emerald-400 "
+        className="group rounded-xl py-2 px-3 shadow-emerald-400/20 hover:scale-[1.03] hover:bg-emerald-400/80 transition-all shadow-md bg-emerald-400 "
         onClick={onCreateClient}
       >
         <RiAddFill className="text-2xl" />
+        <span class="absolute hidden group-hover:flex -left-3 -top-2 -translate-y-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+          Agregar
+        </span>
       </button>
       <button
-        className=" rounded-xl py-2 px-3 shadow-indigo-400/20 hover:scale-[1.03] hover:bg-indigo-400/60 transition-all shadow-md bg-indigo-400 "
+        className="group rounded-xl py-2 px-3 shadow-indigo-400/20 hover:scale-[1.03] hover:bg-indigo-400/60 transition-all shadow-md bg-indigo-400 "
         onClick={() => setShowModal(true)}
       >
         <RiEdit2Line className="text-2xl" />
+        <span class="absolute hidden group-hover:flex -left-1 -top-2 -translate-y-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+          Editar
+        </span>
       </button>
       <button
-        className=" rounded-xl py-2 px-3 shadow-gray-500/20 hover:scale-[1.03] hover:bg-gray-500/60 transition-all shadow-md bg-gray-500 "
+        className="group rounded-xl py-2 px-3 shadow-gray-500/20 hover:scale-[1.03] hover:bg-gray-500/60 transition-all shadow-md bg-gray-500 "
         onClick={onDisabled}
       >
         <RiUserUnfollowLine className="text-2xl" />
+        <span class="absolute hidden group-hover:flex -left-6 -top-2 -translate-y-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+          Deshabilitar
+        </span>
       </button>
       <button
-        className=" rounded-xl py-2 px-3 shadow-green-400/20 hover:scale-[1.03] hover:bg-green-400/60 transition-all shadow-md bg-green-400 "
+        className="group rounded-xl py-2 px-3 shadow-green-400/20 hover:scale-[1.03] hover:bg-green-400/60 transition-all shadow-md bg-green-400 "
         onClick={onEnabled}
       >
         <RiUserFollowLine className="text-2xl" />
+        <span class="absolute hidden group-hover:flex -left-3 -top-2 -translate-y-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+          Habilitar
+        </span>
       </button>
       <CreateClient isVisible={showModal} onClose={() => setShowModal(false)} />
     </section>
--- src/components/bossComponents/BottomsActions.jsx
+++ src/components/bossComponents/BottomsActions.jsx
@@ -16,22 +16,31 @@ const BottomsActions = () => {
   return (
     <section className=" text-white text-bold flex justify-evenly w-full  items-center rounded-md flex-wrap gap-4 pt-8">
       <button
-        className=" rounded-xl py-2 px-3 shadow-orange-400/20 hover:scale-[1.03] hover:bg-orange-400/60 transition-all shadow-md bg-orange-400 "
+        className="group rounded-xl py-2 px-3 shadow-orange-400/20 hover:scale-[1.03] hover:bg-orange-400/60 transition-all shadow-md bg-orange-400 "
         onClick={onPromoteVip}
       >
         <RiVipCrown2Line className="text-2xl" />
+        <span class="absolute hidden group-hover:flex -left-3 -top-2 -translate-y-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+          Agregar VIP
+        </span>
       </button>
       <button
-        className=" rounded-xl py-2 px-3 shadow-gray-400/20 hover:scale-[1.03] hover:bg-gray-400/60 transition-all shadow-md bg-gray-400 "
+        className="group rounded-xl py-2 px-3 shadow-gray-400/20 hover:scale-[1.03] hover:bg-gray-400/60 transition-all shadow-md bg-gray-400 "
         onClick={OnUnsubscribeVip}
       >
         <RiVipCrown2Line className="text-2xl" />
+        <span class="absolute hidden group-hover:flex -left-3 -top-2 -translate-y-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+          Remover VIP
+        </span>
       </button>
       <button
-        className=" rounded-xl py-2 px-3 shadow-sky-400/20 hover:scale-[1.03] hover:bg-sky-400/60 transition-all shadow-md bg-sky-400"
+        className="group rounded-xl py-2 px-3 shadow-sky-400/20 hover:scale-[1.03] hover:bg-sky-400/60 transition-all shadow-md bg-sky-400"
         onClick={() => setIsShow(true)}
       >
         <RiFileListLine className="text-2xl" />
+        <span class="absolute hidden group-hover:flex -left-2 -top-2 -translate-y-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+          Tareas
+        </span>
       </button>
       {isShow && <ModalHistory onClose={() => setIsShow(false)} />}
     </section>

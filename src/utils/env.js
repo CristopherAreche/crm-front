dd0e0402da07ff6bd35cbf213486c56e402f331e
Macarr <rodriguezruccimacarena@gmail.com>
2023-04-16 18:24:06 -0300

    Arreglos y mas arreglos y mas arreglos

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,2 +1,6 @@
-const URL = "https://crm.up.railway.app/api";
+const URL = "https://crm2.up.railway.app/api";
 export default URL;
+
+//localHost const URL = "https://crm.up.railway.app/api";
+
+//deploy const URL = "https://crm2.up.railway.app/api";

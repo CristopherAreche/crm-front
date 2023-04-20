ad8a64efbad2870547af3c48e79c787d4d0d6dcb
Matias <matiasc.02.2003@gmail.com>
2023-04-20 17:17:56 -0300

    Styles in buttons

--- src/components/FilterTop.jsx
+++ src/components/FilterTop.jsx
@@ -4,7 +4,9 @@ import {
   sortVipClients,
   sortEnabledClients,
   sortPurchases,
+  resetClients,
 } from "../app/features/clientSlice";
+import { RiArrowGoForwardFill } from "react-icons/ri";
 
 const FilterTop = () => {
   const dispatch = useDispatch();
@@ -58,16 +60,22 @@ const FilterTop = () => {
     dispatch(sortPurchases({ orderP }));
   };
 
-  // const handleClearFilters = () => {
-  //   dispatch(resetClients());
-  //   document
-  //     .querySelectorAll("select")
-  //     .forEach((select) => (select.value = "todos"));
-  // };
+  const handleClearFilters = () => {
+    dispatch(resetClients());
+  };
 
   return (
     <section className="text-white text-bold flex justify-evenly w-full  items-center py-2 border-b-2 border-light/10 gap-4 flex-row flex-wrap lg:flex-nowrap  ">
       <div className="flex gap-x-2 items-center">
+      <button
+          className="bg-slate-600 rounded-full p-2 group relative mr-12"
+          onClick={handleClearFilters}
+        >
+          <RiArrowGoForwardFill />
+          <span className="absolute hidden group-hover:flex -left-3 -top-2 -translate-y-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+          Reiniciar Filtros
+        </span>
+        </button>
         <p className="text-gray-300 text-sm font-medium">Alfabéticamente:</p>
         <select
           name=""

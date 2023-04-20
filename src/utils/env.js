307a9f91192dec76dfd4096448d3d8221d20f532
Gxspxr <sernagas1234@gmail.com>
2023-04-20 01:40:30 -0300

    login por 3ros arregladoo

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,6 @@
 //Localhost
-const URL = "https://crm.up.railway.app/api";
+// const URL = "https://crm.up.railway.app/api";
 
 //Deploy
-// const URL = "https://crm2.up.railway.app/api";
+const URL = "https://crm2.up.railway.app/api";
 export default URL;
